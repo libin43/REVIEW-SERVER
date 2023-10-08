1c202d59014a330f4a8b531542526a9e456c4071
Libin Biji <libinbiji43@gmail.com>
2023-10-09 01:43:44 +0530

    review result

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,15 +1,29 @@
 import reviewmodel from "../model/review.js";
 
 const userController = {
-    createNewReview: (req, res) =>{
+    createNewReview: (req, res) => {
         console.log(req.body);
 
-        reviewmodel.create(req.body).then((review)=>{
+        reviewmodel.create(req.body).then((review) => {
             res.status(200).json({ success: true, message: 'New Review Added Successfully', review })
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log(err);
             res.status(500).json({ error: true, message: 'Internal Server Error' });
         })
+    },
+
+    viewReviewResults: async (req, res) => {
+        try {
+            let reviewResult = await reviewmodel.find()
+            if(reviewResult.length==0){
+                reviewResult = 'No records'
+            }
+            console.log(reviewResult);
+            res.status(200).json({ success: true, message: 'Review Result Fetched Successfully', reviewResult })
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: "something went wrong" })
+        }
     },
 
     // viewBooks: async (req, res)=>{
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import userController from '../controllers/user.js'
 const userRouter = express.Router()
-const {createNewReview} = userController
+const {
+    createNewReview,
+    viewReviewResults,
+} = userController
 
 
 userRouter.post('/review_submit', createNewReview)
+userRouter.get('/review_result', viewReviewResults)
 // userRouter.get('/viewbook')
 
 export default userRouter;
